0d2d931f1b3d203dc4f9b5dfc76243c6f6235f26
Vysakh Premkumar <84713473+tellmeY18@users.noreply.github.com>
2025-07-10 22:14:47 +0530

    switched to mac

--- pkgs/care/default.nix
+++ pkgs/care/default.nix
@@ -1,65 +1,35 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, mach-nix, ... }:
 
 let
-  python = pkgs.python314;
-  pythonPackages = python.pkgs;
-in pythonPackages.buildPythonApplication rec {
   pname = "care";
   version = "3.0.0";
   src = pkgs.fetchFromGitHub {
     owner  = "ohcnetwork";
     repo   = "care";
     rev    = "v${version}";
-    sha256 =  "sha256-B7d+hiNYDVSDicukVakTl4g3d6dz8uEWy9skzlrfw5U=";
+    sha256 = "sha256-B7d+hiNYDVSDicukVakTl4g3d6dz8uEWy9skzlrfw5U=";
   };
+  pythonVersion = "3.14";
+in
+mach-nix.buildPythonApplication {
+  inherit pname version src;
+  python = pythonVersion;
+  # Use Pipfile.lock if available, else Pipfile, else requirements.txt
+  requirements = "Pipfile.lock";
+  # If you want to use Pipfile or requirements.txt instead, change above line accordingly.
 
-  # Use pip to install all dependencies from Pipfile or requirements.txt in the source repo
-  format = "other";
-  # pip and pipenv are required for install
-  propagatedBuildInputs = [ pythonPackages.pip pkgs.pipenv ];
+  # Optionally, you can set extraBuildInputs if you need system libraries
+  # extraBuildInputs = [ pkgs.postgresql pkgs.redis ];
 
-  # If the repo does not contain requirements.txt, you can generate it from Pipfile.lock
-  # Here we assume requirements.txt or Pipfile/Pipfile.lock is present in the repo
-
-  installPhase = ''
-    runHook preInstall
-
-    # Prefer requirements.txt, fallback to Pipfile if needed
-    if [ -f requirements.txt ]; then
-      reqfile=requirements.txt
-    elif [ -f Pipfile.lock ]; then
-      # Use pipenv to generate requirements.txt from Pipfile.lock
-      ${pkgs.pipenv}/bin/pipenv lock --requirements > requirements.txt
-      reqfile=requirements.txt
-    elif [ -f Pipfile ]; then
-      # Use pipenv to generate requirements.txt from Pipfile
-      ${pkgs.pipenv}/bin/pipenv lock --requirements > requirements.txt
-      reqfile=requirements.txt
-    else
-      echo "No requirements.txt or Pipfile found!"
-      exit 1
-    fi
-
-    # Install all dependencies with pip
-    ${pythonPackages.pip}/bin/pip install --prefix=$out --no-cache-dir -r $reqfile
-
-    mkdir -p $out/lib/care
-    cp -r . $out/lib/care
-    chmod +x $out/lib/care/manage.py
-
-    makeWrapper $out/lib/care/manage.py $out/bin/care-manage \
-      --prefix PYTHONPATH : "$PYTHONPATH" \
+  # Post-install: create wrappers for manage.py, gunicorn, and celery
+  postInstall = ''
+    mkdir -p $out/bin
+    makeWrapper $out/lib/${pname}/manage.py $out/bin/care-manage \
       --set DJANGO_SETTINGS_MODULE config.settings.staging
-
-    makeWrapper ${lib.getExe pythonPackages.gunicorn} $out/bin/care-gunicorn \
-      --prefix PYTHONPATH : "$PYTHONPATH" \
+    makeWrapper ${pkgs.gunicorn}/bin/gunicorn $out/bin/care-gunicorn \
       --set DJANGO_SETTINGS_MODULE config.settings.staging
-
-    makeWrapper ${lib.getExe pythonPackages.celery} $out/bin/care-celery \
-      --prefix PYTHONPATH : "$PYTHONPATH" \
+    makeWrapper ${pkgs.celery}/bin/celery $out/bin/care-celery \
       --set DJANGO_SETTINGS_MODULE config.settings.staging
-
-    runHook postInstall
   '';
 
   meta = with lib; {
